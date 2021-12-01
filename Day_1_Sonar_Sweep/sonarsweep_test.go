60f52d877d34199f573c686badcc222c4379931e
Alexander Wirz <alexander.wirz@fino.digital>
2021-12-01 08:31:39 +0100

    wip

--- Day_1_Sonar_Sweep/sonarsweep_test.go
+++ Day_1_Sonar_Sweep/sonarsweep_test.go
@@ -12,6 +12,6 @@ func TestSecondGreaterThanFirst(t *testing.T) {
 func TestSecondSameAsFirst(t *testing.T) {
 	res := CheckIncreased([]int{1, 1})
 	if res != 0 {
-		t.Fatal("expected increase count to be 1")
+		t.Fatal("expected increase count to be 0")
 	}
 }
