c918a46586031e9f42ce882fce22048ce5ce3170
Alexander Wirz <alexander.wirz@fino.digital>
2021-12-01 08:25:31 +0100

    wip

--- Day_1_Sonar_Sweep/sonarsweep_test.go
+++ Day_1_Sonar_Sweep/sonarsweep_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestFoo(t *testing.T) {
+func TestSecondGreaterThanFirst(t *testing.T) {
 	res := CheckIncreased([]int{1, 2})
 	if res != 1 {
 		t.Fatal("expected increase count to be 1")
