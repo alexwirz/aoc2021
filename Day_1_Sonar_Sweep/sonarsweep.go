19e1a972f72c5f8eee5437f720de7d79d84b7a7a
Alexander Wirz <alexander.wirz@fino.digital>
2021-12-01 08:55:34 +0100

    wip

--- Day_1_Sonar_Sweep/sonarsweep.go
+++ Day_1_Sonar_Sweep/sonarsweep.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
 
 func CheckIncreased(measurements []int) int {
 	if len(measurements) < 2 {
@@ -20,4 +25,31 @@ func CheckIncreased(measurements []int) int {
 	}
 
 	return count
+}
+
+func readFile(fname string) (nums []int, err error) {
+	b, err := ioutil.ReadFile(fname)
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(string(b), "\n")
+	// Assign cap to avoid resize on every append.
+	nums = make([]int, 0, len(lines))
+
+	for _, l := range lines {
+		// Empty line occurs at the end of the file when we use Split.
+		if len(l) == 0 {
+			continue
+		}
+		// Atoi better suits the job when we know exactly what we're dealing
+		// with. Scanf is the more general option.
+		n, err := strconv.Atoi(l)
+		if err != nil {
+			return nil, err
+		}
+		nums = append(nums, n)
+	}
+
+	return nums, nil
 }
