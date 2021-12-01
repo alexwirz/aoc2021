bce40ea3b9cf05137991fb4ba464dde029ce4bcf
Alexander Wirz <alexander.wirz@fino.digital>
2021-12-01 08:30:54 +0100

    wip

--- Day_1_Sonar_Sweep/sonarsweep.go
+++ Day_1_Sonar_Sweep/sonarsweep.go
@@ -1,5 +1,18 @@
 package main
 
 func CheckIncreased(measurements []int) int {
-	return len(measurements) - 1
+	var count int = 0
+	var last int = 234324 // TODO: int(^uint(0) >> 1)
+	for i, v := range measurements {
+		if i == 0 {
+			last = v
+			continue
+		}
+
+		if v > last {
+			count++
+		}
+	}
+
+	return count
 }
