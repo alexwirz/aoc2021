21b98d1ed77321a1c2d365336c8d37cb51ea22d6
Alexander Wirz <alexander.wirz@fino.digital>
2021-12-01 08:55:52 +0100

    wip

--- Day_1_Sonar_Sweep/sonarsweep.go
+++ Day_1_Sonar_Sweep/sonarsweep.go
@@ -38,12 +38,9 @@ func readFile(fname string) (nums []int, err error) {
 	nums = make([]int, 0, len(lines))
 
 	for _, l := range lines {
-		// Empty line occurs at the end of the file when we use Split.
 		if len(l) == 0 {
 			continue
 		}
-		// Atoi better suits the job when we know exactly what we're dealing
-		// with. Scanf is the more general option.
 		n, err := strconv.Atoi(l)
 		if err != nil {
 			return nil, err
