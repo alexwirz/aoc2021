b01698df45eecc86b2e3f49f51a487ccb645b0f9
Alexander Wirz <alexander.wirz@fino.digital>
2021-12-01 08:24:46 +0100

    wip

--- Day_1_Sonar_Sweep/sonarsweep_test.go
+++ Day_1_Sonar_Sweep/sonarsweep_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestFoo(t *testing.T) {
-	res := CheckIncreased([]int{1})
-	if res != 0 {
+	res := CheckIncreased([]int{1, 2})
+	if res != 1 {
 		t.Fatal("expected increase count to be 1")
 	}
 }
