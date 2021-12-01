f89f1869d04909feef7d23a6fc40fbd16601d8af
Alexander Wirz <alexander.wirz@fino.digital>
2021-12-01 08:53:54 +0100

    wip

--- Day_1_Sonar_Sweep/sonarsweep.go
+++ Day_1_Sonar_Sweep/sonarsweep.go
@@ -3,21 +3,20 @@ package main
 import "fmt"
 
 func CheckIncreased(measurements []int) int {
-	var count int = 0
-	var last int = int(^uint(0) >> 1)
-	fmt.Printf("measurements: %v\n", measurements)
-	for i, v := range measurements {
-		if i == 0 {
-			last = v
-			continue
-		}
+	if len(measurements) < 2 {
+		return 0
+	}
 
-		if v > last {
-			fmt.Printf("%d increase (%d)\n", v, last)
+	var count int = 0
+	var last int = measurements[0]
+	fmt.Printf("measurements: %v\n", measurements)
+	for i := 1; i < len(measurements); i++ {
+		if measurements[i] > last {
+			fmt.Printf("%d increase (%d)\n", measurements[i], last)
 			count++
 		}
 
-		last = v
+		last = measurements[i]
 	}
 
 	return count
