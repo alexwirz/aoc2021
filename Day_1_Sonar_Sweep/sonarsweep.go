39f40ae8730a2ef9380cb1d553a1ad943b0c04b3
Alexander Wirz <alexander.wirz@fino.digital>
2021-12-01 08:54:34 +0100

    wip

--- Day_1_Sonar_Sweep/sonarsweep.go
+++ Day_1_Sonar_Sweep/sonarsweep.go
@@ -21,23 +21,3 @@ func CheckIncreased(measurements []int) int {
 
 	return count
 }
-
-func CheckIncreased2(measurements []int) int {
-	if len(measurements) < 2 {
-		return 0
-	}
-
-	var count int = 0
-	var last int = measurements[0]
-	fmt.Printf("measurements: %v\n", measurements)
-	for i := 1; i < len(measurements); i++ {
-		if measurements[i] > last {
-			fmt.Printf("%d increase (%d)\n", measurements[i], last)
-			count++
-		}
-
-		last = measurements[i]
-	}
-
-	return count
-}
